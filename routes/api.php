6624799444e272de9fd7bda65f598e18b146d863
enesfurkansasmaz <enesfurkansasmaz@gmail.com>
2025-04-01 01:15:12 +0300

    push

--- routes/api.php
+++ routes/api.php
@@ -2,5 +2,14 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\ProductController;
+use App\Http\Controllers\Api\OrderController;
+use App\Http\Controllers\Api\DiscountRuleController;
+
+Route::apiResource('discount-rules', DiscountRuleController::class);
+
 
 Route::apiResource('products', ProductController::class);
+
+Route::get('/orders', [OrderController::class, 'listOrder']);
+Route::post('/orders', [OrderController::class, 'addOrder']);
+Route::delete('/orders/{order}', [OrderController::class, 'deleteOrder']);
